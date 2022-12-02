48135ee7c73d5bc3fb9d744045735b28f087893f
SaveLuciferR <cmudrec@mail.ru>
2022-12-02 20:23:45 +0300

    Добавьте файлы проекта.

---
+++ Task_2_4_Forms/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task_2_4_Forms
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			textBox1.Text = "";
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			textBox1.Text = "";
+
+			int k = 5;
+
+			for (int i = 6; i <= 10; i++)
+			{
+				for (int j = 0; j < k; j++)
+				{
+					textBox1.Text += String.Format(i + "\t");
+				}
+
+				textBox1.Text += String.Format("\r\n");
+				k--;
+			}
+		}
+	}
+}
